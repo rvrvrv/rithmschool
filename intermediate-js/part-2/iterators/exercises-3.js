70392f9fed6dca10b83d902f5613364560676b42
rvrvrv <ibanez19@sbcglobal.net>
2017-08-18 17:12:38 -0500

    Upload completed files

--- intermediate-js/part-2/iterators/exercises-3.js
+++ intermediate-js/part-2/iterators/exercises-3.js
@@ -1,6 +1,7 @@
 import { songs } from './songs';
 
 // Array.forEach
+
 function listNames() {
   console.log(songs);
   songs.forEach(song => console.log(song.name));
@@ -22,6 +23,7 @@ function summerJamCount() {
 }
 
 // Array.map
+
 function getDurations() {
   return songs.map(song => song.duration);
 }
@@ -39,4 +41,46 @@ function getMainArtists() {
     if (song.artist.includes('featuring')) return song.artist.slice(0, song.artist.indexOf('featuring') - 1);
     return song.artist;
   });
+}
+
+// Array.filter
+
+function getBigHits(songArr) {
+  return songArr.filter(song => song.weeksAtNumberOne >= 10);
+}
+
+function getShortSongs(songArr) {
+  return songArr.filter(song => +song.duration.split(':')[0] < 3);
+}
+
+function getSongsByArtist(songArr, artist) {
+  return songArr.filter(song => song.artist.toLowerCase() === artist.toLowerCase().trim());
+}
+
+// Array.reduce
+
+function summerJamCount2(songArr) {
+  return songArr.reduce((acc, next) => {
+    if (next.month > 5 && next.month < 9) acc++
+    return acc;
+  }, 0);
+}
+
+function getTotalDurationInSeconds(songArr) {
+  return getDurationInSeconds(songArr).reduce((acc, next) => acc + next, 0);
+}
+
+function getSongCountByArtist(songArr) {
+  return songArr.reduce((acc, next) => {
+    if (next.artist in acc) acc[next.artist]++;
+    else acc[next.artist] = 1;
+    return acc;
+  }, {});
+}
+
+function averageWeeksAtNumberOne(songArr) {
+  // First, get total number of weeks at #1 for all songs
+  let totalWeeks = songArr.reduce((acc, next) => acc + next.weeksAtNumberOne, 0);
+  // Then, divide totalWeeks by total number of songs for the average
+  return (totalWeeks / songArr.length).toFixed(1);
 }
