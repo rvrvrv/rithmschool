3d666f57026bb7398bb11e63340d55e25767fb09
rvrvrv <ibanez19@sbcglobal.net>
2017-08-27 13:01:01 -0500

    Implement ESLint

--- advanced-js/asynchronousExercises.js
+++ advanced-js/asynchronousExercises.js
@@ -1,35 +1,33 @@
 // Always complete first task before second
 function inOrder(first, second) {
   return new Promise((resolve, reject) => {
+    try {
       resolve(first);
-    })
+    } catch (err) {
+      console.log('An error occurred during the first task.');
+      reject(first);
+    }
+  })
     .then(second);
 }
 
-const logOne = setTimeout(function() {
-  console.log('one!')
-}, Math.random() * 1000)
+const logOne = setTimeout(() => console.log('one!'), Math.random() * 1000);
+const logTwo = setTimeout(() => console.log('two!'), Math.random() * 1000);
 
-const logTwo = setTimeout(function() {
-  console.log('two!')
-}, Math.random() * 1000)
-
-inOrder(logOne, logTwo)
+inOrder(logOne, logTwo);
 
 
 // Retrieve and display a list of all episodes in one season of a TV show
 
 function getEpisodes(seasonId) {
   return fetch(`https://api.tvmaze.com/seasons/${seasonId}/episodes`)
-    .then(res => {
+    .then((res) => {
       if (res.ok) return res.json();
       throw new Error(`Could not retrieve episode data for ID ${seasonId}`);
-    }).then(data => {
-      return episodes = data.reduce((acc, next) => {
-        let title = `<h3>Episode ${next.number}: ${next.name}</h3>`;
-        return acc + title + next.summary;
-      }, '');
-    }).catch(err => console.log(err.message));
+    }).then(data => episodes = data.reduce((acc, next) => {
+      const title = `<h3>Episode ${next.number}: ${next.name}</h3>`;
+      return acc + title + next.summary;
+    }, '')).catch(err => console.log(err.message));
 }
 
 const psychSeason1 = getEpisodes(2016).then(data => console.log(data));
